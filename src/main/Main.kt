809b77e0740010c240058bb4d00c4926df3834ac
Marih Souza <marina-queiroz2011@hotmail.com>
2022-02-21 11:46:55 -0300

    first teste

---
+++ src/main/Main.kt
@@ -0,0 +1,18 @@
+class Main
+println("Marina - Oi amor, como esta?")
+println("Isa - Estou bem vida e você?")
+println("Marina - Tambem estou, o que queria me perguntar?")
+println("Isa - Ah sim amor, você já fez a reserva?")
+println("Marina - Claro amor, as 19hs esta okay")
+println("Isa - Perfeito vida, esta ansiosa?")
+println("Marina - Claro linda, quer pedir o de sempre?")
+println("Isa - Hoje eu quero inovar")
+println("Marina - Uhm perfeito, vou com você ")
+println("Isa - Vamos de carro?")
+println("Marina - Vamos né, porque sei da sua preguiça")
+println("Isa - Que isso? Que calúnia!!")
+println("Marina - Tu sabe que é verdade haha")
+println("Isa - Ai ai ! tabom ta bom ")
+println("Marina - haha até mais tarde amor, beijos")
+println("Isa - Beijos, até") {
+}
